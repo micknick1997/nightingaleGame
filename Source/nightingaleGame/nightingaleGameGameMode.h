f7c80c0edaec600bccd8a0cbd1420caceaade692
micknick1997 <micknick1997@gmail.com>
2018-09-08 16:23:32 -0400

    Initial commit

---
+++ Source/nightingaleGame/nightingaleGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "nightingaleGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AnightingaleGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AnightingaleGameGameMode();
+};
+
+
+
